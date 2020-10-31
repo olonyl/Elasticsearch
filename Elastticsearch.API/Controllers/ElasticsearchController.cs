356b7556f563395430b857655fc430b98e8e9c7a
olonyl <o.rocha@contolloconsulting.com>
2020-10-30 23:12:15 -0600

    Project structure refactored

--- Elastticsearch.API/Controllers/ElasticsearchController.cs
+++ Elastticsearch.API/Controllers/ElasticsearchController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Elasticsearch.Service.DTO;
 using Elasticserach.Service.Interfaces;
 using Elastticsearch.API.Controllers.Base;
